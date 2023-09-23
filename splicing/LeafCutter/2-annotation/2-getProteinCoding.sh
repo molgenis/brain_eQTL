e2e0ace0ded2b44a3b6275e3ab42980f18c5b7f0
Harm-Jan Westra <westra.harmjan@outlook.com>
2023-09-23 12:10:40 +0000

    git is complicated

--- splicing/LeafCutter/2-annotation/2-getProteinCoding.sh
+++
@@ -1,4 +0,0 @@
-python getProteinCoding.py \
-	./output/annotation_leafcutter.txt.gz \
-	./output/annotation_leafcutter_protein_codin.txt.gz \
-	/groups/umcg-biogen/tmp01/annotation/GeneReference/GencodeV32/gencode.v32.primary_assembly.annotation.collapsedGenes.proteincoding.txt.gz
